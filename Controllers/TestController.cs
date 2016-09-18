596bcbfec907c7d1c123bb27e64361a506886a49
devimaya <evie.oath@gmail.com>
2016-09-18 19:52:12 +0700

    more logic, attempting to add view

--- Controllers/TestController.cs
+++ Controllers/TestController.cs
@@ -21,7 +21,7 @@ namespace Devi.ParkingService.Controllers
                 Name="P1",
                 Id=1,
                 LocationId=1,
-                Capacity=1
+                Capacity=2
             });
             areaList.Add(new Area{
                 Name="P2",
@@ -40,13 +40,16 @@ namespace Devi.ParkingService.Controllers
             });
         }
 
+
         public JsonResult AvailableArea(int inLocId)
         {
             //inputs
             //Location inLoc = locationList[0];
-            DateTime inStart= new DateTime(2016,9,23,20,30,38);
-            DateTime inEnd = new DateTime(2016,9,23,12,0,39);
+            DateTime inStart= new DateTime(2016,9,23,12,30,38);
+            DateTime inEnd = new DateTime(2016,9,23,18,0,0);
             //endinput
+
+            List<Area> result = new List<Area>();
 
             //fetch areas that is in the inputted location
             for(int i=0; i<areaList.Count; i++)
@@ -61,21 +64,42 @@ namespace Devi.ParkingService.Controllers
                     for(int j=0; j<bookingList.Count; j++)
                     {
                         //area check
+                        //  Query : SELECT * FROM booking WHERE Id=inLocId
                         if (areaList[i].Id == bookingList[j].AreaId)
                         {
-                            avCount++;
+                            if(!checkTime(bookingList[j], inStart, inEnd)) avCount++;
                         }
-
                     }
-
-                    if (avCount<areaList[i].Capacity) return Json(areaList[i]);
-
+                    
+                    if (avCount<areaList[i].Capacity) result.Add(areaList[i]);
 
                 }
             }
-            return Json("not found");
+            if (result.Count==0)
+                return Json("not found");
+            else
+                return Json(result);
 
             
+        }
+
+        //fix inputted's time according to a time unit
+        public DateTime fixTime()
+        {
+            DateTime input = new DateTime(2016,10,23,12,0,39);
+
+            //putting this here now, ill implement it properly later
+            //values are in minutes
+            int timeUnit = 30;
+            int gracePeriod = 10;
+
+            int min = input.Minute%timeUnit;
+            if (min==0) min=0;
+            else min=min*timeUnit;
+            
+
+
+            return input;
         }
 
         //check between a booking's start and end value with
@@ -95,6 +119,7 @@ namespace Devi.ParkingService.Controllers
             return true; //no collision, slot can be used
             else return false; //slot is used
         }
+
         public JsonResult Test()
         {
             
@@ -106,21 +131,18 @@ namespace Devi.ParkingService.Controllers
 
             return Json(checkTime(book1, start, end));
 
-            /*
-            //input start and end must both either BEFORE/AFTER the booking
-            int valueA, valueB, valueC, valueD;
+        }
 
-            valueA = DateTime.Compare(start, book1.start);
-            valueB = DateTime.Compare(start, book1.end);
-            valueC = DateTime.Compare(end, book1.start);
-            valueD = DateTime.Compare(end, book1.end);
+        //view practice etc
+        
+        public ViewResult Index()
+        {
+            return View();
+        }
 
-            if (valueA == valueB &&
-                valueC == valueD &&
-                valueA == valueC)
-            return Json("true");
-            else return Json("false");*/
-
+        public ActionResult DisplayBookings()
+        {
+            return View();
         }
 
     }
