9200d6e981ce9eb6aea9b12786570204006e6336
devimaya <evie.oath@gmail.com>
2016-09-21 18:23:36 +0700

    hit a bit of a snag x.x previous result is carried onto the next function call. the first function call results correctly though.

--- Controllers/TestController.cs
+++ Controllers/TestController.cs
@@ -38,17 +38,28 @@ namespace Devi.ParkingService.Controllers
                 start=new DateTime(2016,9,23,12,0,39),
                 end=new DateTime(2016,9,23,22,0,39)
             });
+            bookingList.Add(new Booking{
+                Id=2,
+                CustomerId=2,
+                CarId=2,
+                AreaId=1,
+                LocationId=1,
+                start=new DateTime(2016,9,23,12,0,39),
+                end=new DateTime(2016,9,23,22,0,39)
+            });
+            bookingList.Add(new Booking{
+                Id=3,
+                CustomerId=1,
+                CarId=1,
+                AreaId=1,
+                LocationId=1,
+                start=new DateTime(2016,8,23,12,0,39),
+                end=new DateTime(2016,8,23,22,0,39)
+            });
         }
 
-
-        public JsonResult AvailableArea(int inLocId)
+        /*public List<Area> GetArea(int inLocId, DateTime inStart, DateTime inEnd)
         {
-            //inputs
-            //Location inLoc = locationList[0];
-            DateTime inStart= new DateTime(2016,9,23,12,30,38);
-            DateTime inEnd = new DateTime(2016,9,23,18,0,0);
-            //endinput
-
             List<Area> result = new List<Area>();
 
             //fetch areas that is in the inputted location
@@ -75,31 +86,81 @@ namespace Devi.ParkingService.Controllers
 
                 }
             }
-            if (result.Count==0)
-                return Json("not found");
-            else
-                return Json(result);
+            return result;
 
-            
+        }*/
+
+
+        public JsonResult AvailableArea()
+        {
+
+            //Get from form index.cshtml
+            //string var1 = Request.Form["inputId"];
+            //int inLocId = Int32.Parse(var1);
+            //int inCarId = Int32.Parse(Request.Form["custId"]);
+            //int inCustId = Int32.Parse(Request.Form["carId"]);
+
+
+            //hardcoded inputs
+            int inLocId = 1;
+            DateTime inStart= new DateTime(2016,9,24,12,30,38);
+            DateTime inEnd = new DateTime(2016,9,24,18,0,0);
+            //endinput
+
+            List<Area> result = new List<Area>();
+
+            //fetch areas that is in the inputted location
+            for(int i=0; i<areaList.Count; i++)
+            {
+                int avCount=0;
+
+                if (inLocId == areaList[i].LocationId)
+                {
+                    //for each booking that 'collides' with the input, avCount++
+                    //area is available if avCount<area.Capacity
+                    avCount=0;
+
+                    //loop bookingList, check each booking if it collides with input
+                    for(int j=0; j<bookingList.Count; j++)
+                    {
+                        //area check
+                        //  Query : SELECT * FROM booking WHERE Id=inLocId
+                        if (areaList[i].Id == bookingList[j].AreaId)
+                        {
+                            if(!checkTime(bookingList[j], inStart, inEnd)) avCount++;
+                        }
+                    }
+                    
+                    if (avCount<areaList[i].Capacity) result.Add(areaList[i]);
+
+                }
+            }
+
+            return Json(result);
         }
 
         //fix inputted's time according to a time unit
-        public DateTime fixTime()
+        public DateTime fixStartTime()
         {
             DateTime input = new DateTime(2016,10,23,12,0,39);
 
             //putting this here now, ill implement it properly later
             //values are in minutes
             int timeUnit = 30;
-            int gracePeriod = 10;
 
-            int min = input.Minute%timeUnit;
-            if (min==0) min=0;
-            else min=min*timeUnit;
-            
+            int min;
+            if (timeUnit<60)
+            {
+                min = input.Minute%timeUnit;
+                if (min==0) 
+                    min=0;
+                else 
+                    min=min*timeUnit;
+            }
+            else 
+                min=0;
 
-
-            return input;
+            return new DateTime(input.Year, input.Month, input.Day, input.Hour, min, 0);
         }
 
         //check between a booking's start and end value with
