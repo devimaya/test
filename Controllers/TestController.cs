e6f1f0bbfea6e79097b06fe6fb4a619b50659801
devimaya <evie.oath@gmail.com>
2016-09-23 04:16:07 +0700

    created simple form for booking & lists available area from the inputted form

--- Controllers/TestController.cs
+++ Controllers/TestController.cs
@@ -63,7 +63,7 @@ namespace Devi.ParkingService.Controllers
             
         }
 
-        /*public List<Area> GetArea(int inLocId, DateTime inStart, DateTime inEnd)
+        public List<Area> GetArea(int inLocId, DateTime inStart, DateTime inEnd)
         {
             List<Area> result = new List<Area>();
 
@@ -93,56 +93,76 @@ namespace Devi.ParkingService.Controllers
             }
             return result;
 
-        }*/
+        }
 
-
-        public JsonResult AvailableArea()
+        public ActionResult AvailableArea()
         {
 
             //Get from form index.cshtml
-            //string var1 = Request.Form["inputId"];
-            //int inLocId = Int32.Parse(var1);
-            //int inCarId = Int32.Parse(Request.Form["custId"]);
-            //int inCustId = Int32.Parse(Request.Form["carId"]);
+            string var1 = Request.Form["inputId"];
+            int inLocId = Int32.Parse(var1);
+            int inCarId = Int32.Parse(Request.Form["carId"]);
+            int inCustId = Int32.Parse(Request.Form["custId"]);
 
+            DateTime inStart = Convert.ToDateTime(Request.Form["instart"]);
+            inStart = inStart.Add(new TimeSpan(Int32.Parse(Request.Form["instarthour"]), Int32.Parse(Request.Form["instartminute"]), 0));
+
+            DateTime inEnd = Convert.ToDateTime(Request.Form["inend"]);
+            inEnd = inEnd.Add(new TimeSpan(Int32.Parse(Request.Form["inendhour"]), Int32.Parse(Request.Form["inendminute"]), 0));
 
             //hardcoded inputs
-            int inLocId = 1;
-            DateTime inStart= new DateTime(2016,9,24,12,30,38);
-            DateTime inEnd = new DateTime(2016,9,24,18,0,0);
+            //int inLocId = 1;
+            //DateTime inStart= new DateTime(2016,9,24,12,30,38);
+            //DateTime inEnd = new DateTime(2016,9,24,18,0,0);
             //endinput
 
-            List<Area> result = new List<Area>();
+            List<Area> result = GetArea(inLocId, inStart, inEnd);
 
-            //fetch areas that is in the inputted location
-            for(int i=0; i<areaList.Count; i++)
-            {
-                int avCount=0;
+            if (result.Count==0) return Json("not found");
 
-                if (inLocId == areaList[i].LocationId)
-                {
-                    //for each booking that 'collides' with the input, avCount++
-                    //area is available if avCount<area.Capacity
-                    avCount=0;
+            Booking newBooking = new Booking{
+                Id=-1,
+                CustomerId=inCustId,
+                CarId=inCarId,
+                AreaId=-1,
+                LocationId=inLocId,
+                start=inStart,
+                end=inEnd
+            };
 
-                    //loop bookingList, check each booking if it collides with input
-                    for(int j=0; j<bookingList.Count; j++)
-                    {
-                        //area check
-                        //  Query : SELECT * FROM booking WHERE Id=inLocId
-                        if (areaList[i].Id == bookingList[j].AreaId)
-                        {
-                            if(!checkTime(bookingList[j], inStart, inEnd)) avCount++;
-                        }
-                    }
-                    
-                    if (avCount<areaList[i].Capacity) result.Add(areaList[i]);
-
-                }
-            }
-
-            return Json(result);
+            //return Json(result);
+            return RedirectToAction("ChooseArea", new {Id=-1,
+                CustomerId=inCustId,
+                CarId=inCarId,
+                AreaId=-1,
+                LocationId=inLocId,
+                start=inStart,
+                end=inEnd});
         }
+
+        public ActionResult ChooseArea(Booking nBk)
+        {
+            List<Area> avArea = GetArea(nBk.LocationId, nBk.start, nBk.end);
+
+            return View(avArea);
+        }
+
+        [HttpPost]
+        public ActionResult SaveBooking(Booking nBk)
+        {
+            bookingList.Add(new Booking{
+                Id=bookingList.Count,
+                CustomerId=nBk.CustomerId,
+                CarId=nBk.CarId,
+                AreaId=nBk.AreaId,
+                LocationId=nBk.LocationId,
+                start=nBk.start,
+                end=nBk.end,
+            });
+
+            return Json("done");
+        }
+
 
         //fix inputted's time according to a time unit
         public DateTime fixStartTime()
